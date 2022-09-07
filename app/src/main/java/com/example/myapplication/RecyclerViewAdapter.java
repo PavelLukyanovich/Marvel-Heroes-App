0512931846c638855122424a935608f2f39f488d
Pavel Lukyanovich <paellukyanovich1@gmail.com>
2022-09-08 02:13:30 +0300

    Create RecyclerViewAdapter class

---
+++ app/src/main/java/com/example/myapplication/RecyclerViewAdapter.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        public RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+}
